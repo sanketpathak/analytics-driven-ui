4620ae4ff35c4bb9ff9610cb22aeb2dba60cb30f
Sanket Pathak <spathak@localhost.localdomain>
2017-12-15 12:36:55 +0530

    Adding application component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ApplicationComponent } from './application/application.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule
