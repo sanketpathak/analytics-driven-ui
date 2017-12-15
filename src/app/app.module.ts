80f4c9700a4ba672af64951d25d449fc7628b1dc
rratnawa <rratnawa@redhat.com>
2017-12-15 12:35:55 +0530

    add package component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PackagesComponent } from './packages/packages.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PackagesComponent
   ],
   imports: [
     BrowserModule
