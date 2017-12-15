5ef65c13490edaacff7ea2bbd330ee41dcd2c297
Sanket Pathak <spathak@localhost.localdomain>
2017-12-15 19:24:41 +0530

    intermediate changes

--- src/app/application/application.component.ts
+++ src/app/application/application.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
-  styleUrls: ['./application.component.css']
+  styleUrls: ['./application.component.less']
 })
 export class ApplicationComponent implements OnInit {
-
+  public showtd: boolean = false;
+  public symbol: string = 'fa fa-angle-right';
+  public trial: any;
   constructor() { }
 
   ngOnInit() {
   }
-
+  private toggle_td() {
+    this.showtd = !this.showtd;
+    if (this.showtd === true) {
+        this.symbol = 'fa fa-angle-down';
+    }
+    else {
+        this.symbol = 'fa fa-angle-right';
+    }
+  }
 }
